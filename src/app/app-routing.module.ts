5d5320bfa1b541ab69463e3e5a2cb9f81ffc3b4b
Himanshu Singh <singhhars1110@gmail.com>
2022-09-26 16:30:55 +0530

    added education component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
 import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {path:'',
    component:ProfileComponent},
-   {path:'about',component:AboutComponent}
+   {path:'about',component:AboutComponent},
+   {path:'education',component:EducationComponent}
 
 ];
 
