74655ced3d85f11c8e03cb6e298c2eedde8abc89
Himanshu Singh <singhhars1110@gmail.com>
2022-09-26 20:00:42 +0530

    component added to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
 import { ProfileComponent } from './profile/profile.component';
+import { SkillsComponent } from './skills/skills.component';
 
 const routes: Routes = [
   {path:'',
    component:ProfileComponent},
    {path:'about',component:AboutComponent},
-   {path:'education',component:EducationComponent}
+   {path:'education',component:EducationComponent},
+   {path:'skills', component:SkillsComponent},
+   {path:'experience',component:ExperienceComponent},
+   {path:'contact',component:ContactComponent}
 
 ];
 
